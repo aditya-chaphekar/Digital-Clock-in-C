0689f42bf45b90e22861f4353dadbddca17b1ece
senor-7r4c3r <42082316+senor-7r4c3r@users.noreply.github.com>
2019-08-14 11:11:44 +0530

    Add files via upload

---
+++ DIGI_C~1.C
@@ -0,0 +1,85 @@
+/*
+
+Title: Digital Clock in C.
+Author: Tr4cEr
+
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<time.h>
+#include<string.h>
+#include<stdlib.h>
+
+void main()
+{
+	int hr=0,min=0, sec=0,idxToDel=0,i,result;
+	char current[30];
+	time_t rawtime;
+	struct tm * timeinfo;
+	time (&rawtime);
+	timeinfo = localtime(&rawtime);
+	strcpy(current,asctime(timeinfo));
+	clrscr();
+
+	{
+		for(i=0;i<11;i++)
+			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
+		idxToDel = 3;
+		for(i=0;i<10;i++)
+			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
+		result = atoi( current );
+		hr=result;
+	}
+
+	timeinfo = localtime(&rawtime);
+	strcpy(current,asctime(timeinfo));
+
+	{
+		idxToDel = 0;
+		for(i=0;i<14;i++)
+			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
+		idxToDel = 3;
+		for(i=0;i<7;i++)
+			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
+		result = atoi( current );
+		min=result;
+	}
+
+	timeinfo = localtime(&rawtime);
+	strcpy(current,asctime(timeinfo));
+
+	{
+		idxToDel = 0;
+		for(i=0;i<17;i++)
+			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
+		idxToDel = 3;
+		for(i=0;i<4;i++)
+			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
+		result = atoi( current );
+		sec=result;
+	}
+
+	clrscr();
+	while(!kbhit())
+	{
+		if(hr>12)
+			hr=0;
+		printf("%d : %d : %d",hr,min,sec);
+		delay(1000);
+		clrscr();
+		sec++;
+		if(sec==60)
+		{
+			min++;
+			if(min==60)
+			{
+				hr++;
+				if(hr==12)
+					hr=0;
+				min=0;
+			}
+			sec=0;
+		}
+	}
+	getch();
+}
