95d1d49bb2456e457dc738fc71af43bfcdf81d33
senor-7r4c3r <42082316+senor-7r4c3r@users.noreply.github.com>
2019-08-14 11:21:37 +0530

    Update DIGI_C~1.C

--- DIGI_C~1.C
+++ DIGI_C~1.C
@@ -12,16 +12,20 @@ Author: Tr4cEr
 
 void main()
 {
+	//initializing required variables
 	int hr=0,min=0, sec=0,idxToDel=0,i,result;
 	char current[30];
 	time_t rawtime;
 	struct tm * timeinfo;
+	//getting the date & time from the time library
 	time (&rawtime);
+	//coping the fetched time into current variable
 	timeinfo = localtime(&rawtime);
 	strcpy(current,asctime(timeinfo));
 	clrscr();
-
+	
 	{
+		//this block extracts the current hour of the day by removing additional info.
 		for(i=0;i<11;i++)
 			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
 		idxToDel = 3;
@@ -30,11 +34,11 @@ void main()
 		result = atoi( current );
 		hr=result;
 	}
-
 	timeinfo = localtime(&rawtime);
 	strcpy(current,asctime(timeinfo));
 
 	{
+		//this block extracts the current minute of the day by removing additional info.
 		idxToDel = 0;
 		for(i=0;i<14;i++)
 			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
@@ -49,6 +53,7 @@ void main()
 	strcpy(current,asctime(timeinfo));
 
 	{
+		//this block extracts the current second of the day by removing additional info.
 		idxToDel = 0;
 		for(i=0;i<17;i++)
 			memmove(&current[idxToDel], &current[idxToDel + 1], strlen(current) - idxToDel);
@@ -60,9 +65,13 @@ void main()
 	}
 
 	clrscr();
+	/*
+	the loop below will continue executing until a keyboard hit is detected
+	this loop updates the output screen every second and displays the current time in hh:mm:ss format.
+	*/
 	while(!kbhit())
 	{
-		if(hr>12)
+		if(hr>=12)
 			hr=0;
 		printf("%d : %d : %d",hr,min,sec);
 		delay(1000);
@@ -81,5 +90,4 @@ void main()
 			sec=0;
 		}
 	}
-	getch();
 }
